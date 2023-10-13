c963be926ddb40e45d48d81f1a252c378b4d9c97
Hanerlan <hk.erlandsen@gmail.com>
2023-10-13 11:56:41 +0200

    Added endpoints returning the square root

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ var subtractRouter = require('./routes/subtract');
 var divideRouter = require('./routes/divide');
 var multiplyRouter = require('./routes/multiply');
 var previousRouter = require('./routes/previous');
+var sqrtRouter = require('./routes/sqrt');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -36,6 +37,7 @@ app.use('/subtract', subtractRouter);
 app.use('/divide', divideRouter);
 app.use('/multiply', multiplyRouter);
 app.use('/previous', previousRouter);
+app.use('/sqrt', sqrtRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/previous.js
+++ routes/previous.js
@@ -8,13 +8,13 @@ var jwt = require('jsonwebtoken')
 
 router.use(jsend.middleware);
 
+
 /* GET */
 router.get('/add/:number1', async (req, res, next) => {
     const number1 = parseInt(req.params.number1); // we take number 1 from the route and parse it to an integer
     if(isNaN(number1)) { 
         return res.jsend.fail({"number1": "number1 is not in correct format"});
     }
-
     const token = req.headers.authorization?.split(' ')[1]; 
     if(!token) {
         return res.jsend.fail({"result": "JWT token not provided"});
@@ -26,23 +26,25 @@ router.get('/add/:number1', async (req, res, next) => {
     catch(err) {
         return res.jsend.fail({"result": err});
     }
-
     const previous = await resultService.getOne(decodedToken.id); // if everything works correctly
     if (!previous) { // Handle the case where no result is found for the user.
         return res.jsend.fail({"result": "Previous result not found for the user."});
     }
-
     const result = previous.Value + number1; // we use the result service to get the previous value from the database
     resultService.create("add", result, decodedToken.id); // we then use it to calculate the result 
-    res.jsend.success({"result": result, "previousOperation": previous.OperationName, "previousValue": previous.Value}); // and save it in the database and send the response to the user
+    res.jsend.success({
+        "result": result, 
+        "previousOperation": previous.OperationName, 
+        "previousValue": previous.Value
+    }); // and save it in the database and send the response to the user
 });
+
 
 router.get('/subtract/:number1', async (req, res, next) => {
     const number1 = parseInt(req.params.number1); 
     if(isNaN(number1)) { 
         return res.jsend.fail({"number1": "number1 is not in correct format"});
     }
-
     const token = req.headers.authorization?.split(' ')[1]; 
     if(!token) {
         return res.jsend.fail({"result": "JWT token not provided"});
@@ -54,15 +56,17 @@ router.get('/subtract/:number1', async (req, res, next) => {
     catch(err) {
         return res.jsend.fail({"result": err});
     }
-
     const previous = await resultService.getOne(decodedToken.id);
     if (!previous) { 
         return res.jsend.fail({"result": "Previous result not found for the user."});
     }
-
     const result = previous.Value - number1; 
     resultService.create("subtract", result, decodedToken.id); 
-    res.jsend.success({"result": result, "previousOperation": previous.OperationName, "previousValue": previous.Value});
+    res.jsend.success({
+        "result": result, 
+        "previousOperation": previous.OperationName, 
+        "previousValue": previous.Value
+    });
 });
 
 router.get('/multiply/:number1', async (req, res, next) => {
@@ -70,7 +74,6 @@ router.get('/multiply/:number1', async (req, res, next) => {
     if(isNaN(number1)) { 
         return res.jsend.fail({"number1": "number1 is not in correct format"});
     }
-
     const token = req.headers.authorization?.split(' ')[1]; 
     if(!token) {
         return res.jsend.fail({"result": "JWT token not provided"});
@@ -86,10 +89,13 @@ router.get('/multiply/:number1', async (req, res, next) => {
     if (!previous) { 
         return res.jsend.fail({"result": "Previous result not found for the user."});
     }
-
     const result = previous.Value * number1; 
     resultService.create("multiply", result, decodedToken.id); 
-    res.jsend.success({"result": result, "previousOperation": previous.OperationName, "previousValue": previous.Value}); 
+    res.jsend.success({
+        "result": result, 
+        "previousOperation": previous.OperationName, 
+        "previousValue": previous.Value
+    }); 
 });
 
 router.get('/divide/:number1', async function(req, res, next) {
@@ -100,7 +106,6 @@ router.get('/divide/:number1', async function(req, res, next) {
     if(number1 == 0) {
         return res.jsend.fail({"number1": "number1 cannot be 0"}); 
     }
-
     const token = req.headers.authorization?.split(' ')[1];
     if(!token) {
         return res.jsend.fail({"result": "JWT token not provided"});
@@ -112,19 +117,65 @@ router.get('/divide/:number1', async function(req, res, next) {
     catch(err) {
         return res.jsend.fail({"result": err});
     }
-
     const previous = await resultService.getOne(decodedToken.id); 
     if (!previous) { 
         return res.jsend.fail({"result": "Previous result not found for the user."});
     }
-
     const result = previous.Value / number1; 
     resultService.create("divide", Math.round(result), decodedToken.id); 
     if (Number.isInteger(result)) {
-        res.jsend.success({"result": result, "previousOperation": previous.OperationName, "previousValue": previous.Value});
+        res.jsend.success({
+            "result": result, 
+            "previousOperation": previous.OperationName, 
+            "previousValue": previous.Value
+        });
     }
     else {
-        res.jsend.success({"result": Math.round(result), "previousOperation": previous.OperationName, "previousValue": previous.Value, "message": "Result has been rounded, as it was not an integer."});
+        res.jsend.success({
+            "result": Math.round(result), 
+            "previousOperation": previous.OperationName, 
+            "previousValue": previous.Value, 
+            "message": "Result has been rounded, as it was not an integer."});
+    }
+});
+
+
+/* Task 5 - Square root */
+router.get('/sqrt', async function(req, res, next) {
+    const token = req.headers.authorization?.split(' ')[1];
+    if(!token) {
+        return res.jsend.fail({"result": "JWT token not provided"});
+    }
+    let decodedToken;
+    try {
+        decodedToken = jwt.verify(token, process.env.TOKEN_SECRET );
+    }
+    catch(err) {
+        return res.jsend.fail({"result": err});
+    }
+    const previous = await resultService.getOne(decodedToken.id); 
+    if (!previous) { 
+        return res.jsend.fail({"result": "Previous result not found for the user."});
+    }
+    if (previous.Value < 0) {
+        return res.jsend.fail({"result": "Previous value is negative"})
+    }
+    const result = Math.sqrt(previous.Value); 
+    resultService.create("sqrt", Math.round(result), decodedToken.id); 
+    if (Number.isInteger(result)) {
+        res.jsend.success({
+            "result": result, 
+            "previousOperation": previous.OperationName, 
+            "previousValue": previous.Value
+        });
+    }
+    else {
+        res.jsend.success({
+            "result": Math.round(result), 
+            "previousOperation": previous.OperationName, 
+            "previousValue": previous.Value, 
+            "message": "Result has been rounded, as it was not an integer."
+        });
     }
 });
 
